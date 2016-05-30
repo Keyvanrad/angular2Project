5dc5a626280111f38e8e481167397bb5148d40db
Keyvan <keyvang@gmail.com>
2016-05-30 17:16:58 -0400

    Fixed the comment

--- app/movies.service.ts
+++ app/movies.service.ts
@@ -11,6 +11,6 @@ export class MoviesService{
         return this._http.get('http://api.openweathermap.org/data/2.5/forecast?q=Toronto,us&mode=json&appid=1f58e97dac0c67d6801dfb565e71b0f0')
             .map(res => res.json());
         //http://api.rottentomatoes.com/api/public/v1.0/lists/movies/opening.json?apikey=pjjkpm57xrm7gs63spvvaumt&limit=5&country=ca
-        //http://api.openweathermap.org/data/2.5/forecast?q=Toronto,us&mode=xml&appid=1f58e97dac0c67d6801dfb565e71b0f0
+        //http://api.openweathermap.org/data/2.5/forecast?q=Toronto,us&mode=json&appid=1f58e97dac0c67d6801dfb565e71b0f0
     }
 }
