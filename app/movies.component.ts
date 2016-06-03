6a2684b45d46577b36ee6e9a2034a3e765e64d9e
Keyvan <keyvang@gmail.com>
2016-06-03 17:25:59 -0400

    Apply three different movie list from rotten tomato (free API JSON files)

--- app/movies.component.ts
+++ app/movies.component.ts
@@ -5,10 +5,16 @@ import {MoviesService} from "./movies.service";
     selector: 'movies',
     template: `
         <title>Movies</title>
+        <form>
+            <input type="radio" name="category" class="radio" (click)="getTomatoMovies('opening')" checked> Opening
+            <input type="radio" name="category" (click)="getTomatoMovies('boxoffice')"> Canada Box Office
+            <input type="radio" name="category" (click)="getTomatoMovies('upcoming')"> Upcoming!
+        </form>
         <button style="margin-top: 40px" (click)="getTomatoMovies('opening')">Opening</button>
         <button style="margin-top: 40px" (click)="getTomatoMovies('boxoffice')">Canada Box Office</button>
         <button style="margin-top: 40px" (click)="getTomatoMovies('upcoming')">Upcoming!</button>
-        <p>{{moviesResult}}</p>
+        <!--<p>{{moviesResultList ? moviesResultList[0].alternate_ids.imdb : ""}}</p>-->
+        <p>{{selectedMovie?.title}}</p>
     `,
     providers: [MoviesService]
 })
@@ -16,12 +22,18 @@ import {MoviesService} from "./movies.service";
 export class MoviesComponent {
 
     moviesResult: string;
+    //moviesResultList: {};
+    selectedMovie: {};
 
     constructor(private _mvsService: MoviesService){}
 
     getTomatoMovies(queryType: string) {
         this._mvsService.getMovies(queryType).subscribe(
-            data => this.moviesResult = JSON.stringify(data),
+            data => {
+                this.moviesResult = JSON.stringify(data);
+                //this.moviesResultList = JSON.parse(this.moviesResult).movies;
+                this.selectedMovie = JSON.parse(this.moviesResult).movies[0];
+           },
             error => alert(error),
             () => console.log("Finished")
         );
